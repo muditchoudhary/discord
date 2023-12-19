e0f9b13da2105d0e8225ad1b17c5b577f69df8d1
Mudit Choudhary <cmudit60@gmail.com>
2023-12-19 16:55:24 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+
+	return (
+        <div className="flex flex-col">
+            <p className="text-3xl text-red-800">Hello Discord</p>;      
+            <Button variant="link">Click Me</Button>
+        </div>
+    ) 
+}
