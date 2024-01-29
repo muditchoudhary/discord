819090bba5b8097ee65db72f59eace1568df4c98
Mudit Choudhary <cmudit60@gmail.com>
2024-01-29 21:37:49 +0530

    Add manage members modal

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -43,19 +43,28 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
 			</DropdownMenuTrigger>
 			<DropdownMenuContent className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]">
 				{isModerator && (
-					<DropdownMenuItem onClick={() => onOpen("invite", {server})} className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer">
+					<DropdownMenuItem
+						onClick={() => onOpen("invite", { server })}
+						className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer"
+					>
 						Invite People
 						<UserPlus className="h-4 w-4 ml-auto" />
 					</DropdownMenuItem>
 				)}
 				{isAdmin && (
-					<DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer" onClick={() => onOpen("editServer", {server})}>
+					<DropdownMenuItem
+						className="px-3 py-2 text-sm cursor-pointer"
+						onClick={() => onOpen("editServer", { server })}
+					>
 						Server Settings
 						<Settings className="h-4 w-4 ml-auto" />
 					</DropdownMenuItem>
 				)}
 				{isAdmin && (
-					<DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+					<DropdownMenuItem
+						className="px-3 py-2 text-sm cursor-pointer"
+						onClick={() => onOpen("members", { server })}
+					>
 						Manage Members
 						<Users className="h-4 w-4 ml-auto" />
 					</DropdownMenuItem>
