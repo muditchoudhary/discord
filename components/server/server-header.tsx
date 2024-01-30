38ec47b7d57b1844cec1174e0d5d1443beb8ebc5
Mudit Choudhary <cmudit60@gmail.com>
2024-01-30 18:54:25 +0530

    Made Leave server and delete server modals

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -70,20 +70,29 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
 					</DropdownMenuItem>
 				)}
 				{isModerator && (
-					<DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer" onClick={() => onOpen("createChannel")}>
+					<DropdownMenuItem
+						className="px-3 py-2 text-sm cursor-pointer"
+						onClick={() => onOpen("createChannel")}
+					>
 						Create Channel
 						<PlusCircle className="h-4 w-4 ml-auto" />
 					</DropdownMenuItem>
 				)}
 				{isModerator && <DropdownMenuSeparator />}
 				{isAdmin && (
-					<DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+					<DropdownMenuItem
+						className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
+						onClick={() => onOpen("deleteServer", { server })}
+					>
 						Delete Server
 						<Trash className="h-4 w-4 ml-auto" />
 					</DropdownMenuItem>
 				)}
 				{!isAdmin && (
-					<DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+					<DropdownMenuItem
+						className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
+						onClick={() => onOpen("leaveServer", { server })}
+					>
 						Leave Server
 						<LogOut className="h-4 w-4 ml-auto" />
 					</DropdownMenuItem>
